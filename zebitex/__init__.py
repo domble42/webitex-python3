91aca2163bd422ca18885c3cd44354215c7cfc4b
Dominique Bleus <github@clonix.info>
2018-11-04 16:40:24 +0100

    Create __init__.py

---
+++ zebitex/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__title__   = "zebitex"
+__version__ = "0.0.9 alpha"
+__author__  = "Dominique Bleus <github@clonix.info>"
+__repo__    = 'https://github.com/domble42/zebitex-python3'
+__license__ = 'BSD 3-Clause License'
+
+from .zebitex import (Zebitex, ZebitexError)
