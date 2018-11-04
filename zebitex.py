84dfcb02fecc2bf16442e8072399e2e3195ad937
Dominique Bleus <github@clonix.info>
2018-11-04 08:51:57 +0100

    Update zebitex.py

--- zebitex.py
+++ zebitex.py
@@ -11,24 +11,22 @@ import hmac
 import hashlib
 
 class Zebitex():
-    """ A simple wrapper  """
+    """ A simple wrapper
+    
+    """
 
     def __init__(self, access_key, secret_key, is_staging=False):
         self.access_key = access_key
         self.secret_key = secret_key
         self.url = "https://staging.zebitex.com" if is_staging else "https://zebitex.com"
 
-    def debug(self, method, url, params, header, json_payload):
-        print("<mode=\"debug\">")
-        print("""\tmethod = {}\n\turl = {}\n\tparams = {}\n\theader = {}\n\tjson = {}""".format(method, url, params, header, json_payload))
-        print("</mode>")
-
-    def get_signature_payload(self, verb, path, tonce, params):
+    def get_signature_payload(self, verb, path, tonce, params=None):
         """ The signature_payload consist of uppercased HTTP verb, the API path without the host part, the tonce and all the params in JSON form.
         All separated with a vertical slash."""
         json_params = json.dumps(params) if params else "{}"
-        payload = "{}|{}|{}|{}".format(verb.upper(), path, tonce, json_params)
-        return hmac.new(self.secret_key.encode(), payload.encode(), hashlib.sha256).hexdigest()
+        payload = "{}|{}|{}|{}".format(verb.upper(), path, str(tonce), json_params.replace(' ', ''))
+        signature = hmac.new(self.secret_key.encode(), payload.encode(), hashlib.sha256).hexdigest()
+        return (signature)
 
     def get_authorization_header(self, verb, path, params):
         """ The Authorization header:
@@ -37,7 +35,7 @@ class Zebitex():
             - tonce - 13 digits timestamp
             - signed_params - a semicolon separated list of the param names submitted and signed in the request            
         """
-        tonce = int(time.time()) * 1000
+        tonce = int(time.time() * 1000)
         signature = self.get_signature_payload(verb, path, tonce, params)
         signed_params = ";".join(params.keys()) if params else ""
         authorization_header_format = "ZEBITEX-HMAC-SHA256 access_key={}, signature={}, tonce={}, signed_params={}"
@@ -47,17 +45,12 @@ class Zebitex():
     def make_request(self, level, verb, path, params=None):
         user_agent = {"User-Agent": "zebitex-python3 0.0.1 alpha version"}
         authorization_header = {}
+        params = {k: str(v) for k,v in params.items()} if params else None
         if level == "PRIVATE":
             authorization_header = self.get_authorization_header(verb, path, params)
-        if verb == "POST":
-            json_payload = json.dumps(params)
-            params = None
-        else:
-            json_payload = True
         url = self.url + path
         headers = {**user_agent, **authorization_header}
-        self.debug(verb, url, params, headers, json.dumps(params))
-        return requests.request(verb, url, params=params, headers=headers, json=json_payload)
+        return requests.request(verb, url, params=params, headers=headers, json=True)
 
     def funds(self):
         return self.make_request("PRIVATE", "GET", "/api/v1/funds")
@@ -88,13 +81,26 @@ class Zebitex():
     def cancel_order(self, id_order):
         return self.make_request("PRIVATE", "DELETE", "/api/v1/orders/{}/cancel".format(str(id_order)), {"id": str(id_order)})
 
-    def new_order(self,bid, ask, side, price, amount, market, ord_type):
+    def new_order(self, bid, ask, side, price, amount, market, ord_type):
         query = {"bid":bid, "ask":ask, "side":side, "price":price, "amount":amount, "market":market, "ord_type":ord_type}
         return self.make_request("PRIVATE", "POST", "/api/v1/orders", query)
 
 def test():
-    api = Zebitex(access_key="<YOUT ACCESS KEY>", secret_key="<YOUR SECRET KEY>", is_staging=True)
+    """ """
+    api = Zebitex("<ACCESS KEY>", "<SECRETE KEY>", True)
+    t = api.new_order('ltc', # quote currency
+                'btc', # base currency
+                'ask', # order side (bid or ask)
+                '0.321', # price
+                '0.123', # volume
+                'ltcbtc', # market
+                'limit' # order type
+    )
+    print(t)
+    if str(t) is "200":
+        print(t.text)
     t = api.open_orders()
+    print(t)
     print(t.text)
     return 0
 
