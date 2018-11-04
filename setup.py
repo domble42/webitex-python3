f4001c88ee4c2adb8410eca902b37a36a1da9655
Dominique Bleus <github@clonix.info>
2018-11-04 18:13:12 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name = 'zebitex',
+    version = '0.0.1',
+    author = 'Dominique Bleus',
+    author_email = 'github@clonix.info',
+    maintainer = 'Dominique Bleus',
+    maintainer_email = 'github@clonix.info',
+    packages = ['zebitex'],
+    description = 'Python3 wrapper for zebitex API V1, currently in beta version.',
+    long_description = open('README.md','r').read(),    
+    keywords = ['zebitex', 'exchange', 'cryptocurrency', 'API', 'wrapper'],
+    license = 'BSD 3-Clause License',
+    plateformes = 'ALL',
+    url = 'https://github.com/domble42/zebitex-python3/',
+    download_url = 'https://github.com/domble42/zebitex-python3/archive/master.zip'
+)
