28e768fa9881c2885d5ec0cf538abdd70754f170
Dominique Bleus <github@clonix.info>
2018-11-04 17:44:36 +0100

    Update __init__.py

--- zebitex/__init__.py
+++ zebitex/__init__.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__title__   = "Zebitex's API wrapper"
-__version__ = "0.0.1 beta"
-__author__  = "Dominique Bleus <github@clonix.info>"
-__repo__    = 'https://github.com/domble42/zebitex-python3'
-__license__ = 'BSD 3-Clause License'
+__title__       = "Zebitex's API wrapper"
+__version__     = "0.0.1 beta"
+__author__      = "Dominique Bleus <github@clonix.info>"
+__maintainer__  = 'Dominique Bleus <github@clonix.info>"
+__repo__        = 'https://github.com/domble42/zebitex-python3'
+__license__     = 'BSD 3-Clause License'
 
 from .zebitex import (Zebitex, ZebitexError)
